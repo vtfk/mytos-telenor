77f1564650352924302e9f3fa57bb5a5b2779c12
Rune Moskvil Lyngås <rune.moskvil.lyngaas@vtfk.no>
2022-05-05 15:57:24 +0200

    Added support for updating user(s) in Mytos

--- config.js
+++ config.js
@@ -7,6 +7,8 @@ module.exports = {
   MYTOS: {
     URL_GET_USERS: process.env.MYTOS_URL_GET_USERS,
     URL_GET_USER: process.env.MYTOS_URL_GET_USER,
+    URL_UPDATE_USERS: process.env.MYTOS_URL_UPDATE_USERS,
+    URL_UPDATE_USER: process.env.MYTOS_URL_UPDATE_USER,
     USERNAME: process.env.MYTOS_USERNAME,
     PASSWORD: process.env.MYTOS_PASSWORD
   }
